8dd1e7abed8b8940cb23275d439693e98fc09658
Derrick Mei <derrick.mei.168@gmail.com>
2020-03-02 16:43:08 -0600

    defined basic player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
+        self.items = {}
+
+    def add_item(self, item):
+        self.items[item] = item
+
+    def remove_item(self, item):
+        self.items.pop(item, None)
