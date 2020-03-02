747ebf130f8968ad366bb35d46c58d8e39e2e004
Derrick Mei <derrick.mei.168@gmail.com>
2020-03-02 16:42:44 -0600

    added cardinal directions to rooms and items

--- src/room.py
+++ src/room.py
@@ -1,13 +1,26 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+
 class Room():
-  def __init__(self, name, description):
-    self.name = name
-    self.description = description
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.items = {"knife":"damascus"}
 
-  def __str__(self):
-    return f"{self.__class__.__name__}({self.name}: {self.description})"
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
 
-  def __repr__(self):
-    return f"{self.__class__.__name__}({repr(self.name)} {repr(self.description)})"
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.name}: {self.description})"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({repr(self.name)}, {repr(self.description)})"
+
+    def add_item(self, item):
+        self.items[item] = item
+
+    def remove_item(self, item):
+        self.items.pop(item, None)
